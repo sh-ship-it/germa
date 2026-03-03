7db7a06c31780862bb0b629d2f0928cd81922149
Shubham Gharte <shubhamgharte306@gmail.com>
2026-03-03 21:58:54 +0530

    side bar completed

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -9,7 +9,7 @@ router.post('/signup', signup);
 router.post('/login',login);
 router.post('/logout',logout); 
 
-router.post('/update-profile', protectRoute, updateProfile); 
+router.put('/update-profile', protectRoute, updateProfile); 
 
 router.get('/check', protectRoute, (req, res) => res.status(200).json(req.user));
 
