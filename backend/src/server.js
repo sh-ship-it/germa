4636f0b23804ad55041d656be18908f405a4586d
Shubham Gharte <shubhamgharte306@gmail.com>
2026-04-05 22:23:50 +0530

    ss

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,8 @@ const app = express();
 
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+
 app.use(cors({
   origin: ENV.CLIENT_URL,
   credentials: true,
